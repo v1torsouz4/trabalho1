7b24a63f2e4314cb937773a4478170ce254bbf8c
v1torsouz4 <crossvitor14@gmail.com>
2022-06-30 17:38:06 -0400

    Criando texto botão

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+function Novo(){
+    return(
+        <div className = "centro">
+
+		<div className="botao">
+            <p className="titulo"> PREVIOUS </p>
+
+            <p>
+			JQUERY: DETECT UPDATE OF HTML ELEMENT
+            </p>
+
+            
+         </div>
+
+		 <div className="botao">
+            <p className="titulo"> NEXT </p>
+
+            <p>
+			RAILS + DEVISE WORKING WITH MULTIPLE MODELS
+            </p>
+
+            
+         </div>
+           
+
+         </div>
+    )
+}
+export default Novo;
